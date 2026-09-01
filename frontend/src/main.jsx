d706deb74afc045135ce315b2b76cc86cde6a45b
Nithesh Anbusubramanian <ssnagnithesh@gmail.com>
2026-09-01 20:31:18 +0530

    prepare the backend for render deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -17,8 +17,8 @@ import "swiper/css/pagination";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
