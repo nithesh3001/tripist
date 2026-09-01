a4aa4379e8a4d9f16c37957aacf72bf1c297494c
Nithesh Anbusubramanian <ssnagnithesh@gmail.com>
2026-09-01 23:33:30 +0530

    api correction

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const app = express();
 const allowedOrigins = [
   "https://nithesh3001.github.io",
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:3000",
   "http://localhost:5000",
 ];
